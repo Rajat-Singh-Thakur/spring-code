b77ea5713180832c6dba3655f3e677d2068794d4
rajat <rajat@rajat-HP-Notebook>
2019-12-08 22:16:59 +0530

    Constructor injection Collection <Map>

---
+++ SpringCorePractice/src/com/Spring/Example8/Student.java
@@ -0,0 +1,38 @@
+package com.Spring.Example8;
+
+import java.util.Map;
+
+public class Student {
+	private int id;
+	private String name;
+	private Map<String,String> answer;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Map<String, String> getAnswer() {
+		return answer;
+	}
+	public void setAnswer(Map<String, String> answer) {
+		this.answer = answer;
+	}
+	
+	public Student(int id,String name,Map<String,String>answer) {
+		this.id=id;
+		this.name=name;
+		this.answer=answer;
+	}
+	
+	public void MyMethod() {
+		System.out.println("Q "+id+". "+name+"\n"+answer);
+	}
+
+}
--- SpringCorePractice/src/com/Spring/Example8/Test.java
+++ SpringCorePractice/src/com/Spring/Example8/Test.java
@@ -1,10 +1,15 @@
 package com.Spring.Example8;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class Test {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		ApplicationContext appconn=new ClassPathXmlApplicationContext("ApplicationContext.xml");
+		Student std=(Student)appconn.getBean("studentmap");
+		std.MyMethod();
 	}
 
 }
